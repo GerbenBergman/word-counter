4a0d35759894353f9f86adef25837833b75324ce
Gerben <gerbenbergman@hotmail.com>
2021-08-12 19:33:24 +0200

    added karma config file

---
+++ karma.conf.js
@@ -0,0 +1,20 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', 'karma-typescript'],
+    files: [
+      'src/**/*Spec.ts'
+    ],
+    preprocessors: {
+      "**/*.ts": ['karma-typescript']
+    },
+    reporters: ['progress', 'karma-typescript'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    concurrency: Infinity
+  })
+}
